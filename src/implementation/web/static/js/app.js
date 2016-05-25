6164ed6d303e7bded4e919dca88e2d386d53611b
Sripathi <kammari.sripathi@gmail.com>
2016-05-25 22:06:45 +0530

    Added indentation

--- src/implementation/web/static/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('directoryApp',['ngRoute','directoryApp.controllers']);
-app.config(function($routeProvider){
-  $routeProvider
-    .when('/', {
-      templateUrl : '/static/partials/users.html',
-      controller  : 'users'
-    });
-});
-
--- src/implementation/web/static/js/controllers.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('directoryApp.controllers',[]);
-
-app.controller("users", function($scope,  $http, $routeParams, $route,$window){
-    $scope.role_name = $window.role_name;
-    $http.get("/users").success(function(response){
-	$scope.users = response;
-    });
-
-});
