c1673bf5fd4b3a71a7423fa821fcd9661a6985d0
Sripathi <kammari.sripathi@gmail.com>
2016-05-25 19:54:03 +0530

    Removed assingment part code from boilerplate code

--- src/implementation/web/static/js/app.js
+++ src/implementation/web/static/js/app.js
@@ -1,21 +1,9 @@
-var app = angular.module('directoryApp',['ngRoute','directoryApp.controllers','directoryApp.factories']);
+var app = angular.module('directoryApp',['ngRoute','directoryApp.controllers']);
 app.config(function($routeProvider){
   $routeProvider
     .when('/', {
       templateUrl : '/static/partials/users.html',
       controller  : 'users'
-    })
-    .when('/add-user', {
-      templateUrl : '/static/partials/add-user.html',
-      controller  : 'users'
-    })
-    .when('/view-user/:id', {
-      templateUrl : '/static/partials/view-user.html',
-      controller  : 'view-user'
-    })
-    .when('/edit-user/:id', {
-      templateUrl : '/static/partials/edit-user.html',
-      controller  : 'view-user'
     });
 });
 
